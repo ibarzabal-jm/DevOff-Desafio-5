6f571bb40b666bb3de6814c7702420c6fd6f3eb7
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-12-26 23:41:55 -0300

    Update theme.js

--- pokedex-app/src/theme.js
+++ pokedex-app/src/theme.js
@@ -2,6 +2,23 @@ import { extendTheme, theme } from "@chakra-ui/react";
 
 export default extendTheme({
   colors: {
-    grass: theme.colors.green,
+    grass: "#47CFAF",
+    fire: "#FB6D6C",
+    water: "#74BDFD",
+    dragon: "#8774FF",
+    normal: "#BDBDAE",
+    bug: "#C1D11F",
+    poison: "#AA5DA1",
+    electric: "#FEE63C",
+    ground: "#d3b54a",
+    psychic: "#F562B1",
+    fighting: "#A75545",
+    fairy: "#FAADFF",
+    rock: "#CEBD72",
+    ghost: "#7A76D8",
+    ice: "#96F1FF",
+    steel: "#C4C2DB",
+    flying: "#7AA1FF",
+    dark: "#8A6653",
   },
 });
