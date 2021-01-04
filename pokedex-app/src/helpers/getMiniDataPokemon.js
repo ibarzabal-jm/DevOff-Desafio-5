4210a1edf48aed9cb82733198f6df7239b283cac
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2021-01-04 20:38:09 -0300

    Update getMiniDataPokemon.js

--- pokedex-app/src/helpers/getMiniDataPokemon.js
+++ pokedex-app/src/helpers/getMiniDataPokemon.js
@@ -1,24 +1,9 @@
-// export const getMiniDataPokemon = async (id) => {
-//   let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-
-//   const resp = await fetch(url);
-//   const data = await resp.json();
-
-//   const pokemon = {
-//     id,
-//     name: data.name,
-//     image: `https://pokeres.bastionbot.org/images/pokemon/${id}.png`,
-//     types: data.types.map(({ type }) => type.name),
-//   };
-//   return pokemon;
-// };
-
 export const getMiniDataPokemon = async (id) => {
   let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
   const pokemon = {
     id,
-    name: null,
     image: `https://pokeres.bastionbot.org/images/pokemon/${id}.png`,
+    name: null,
     types: [],
   };
 
