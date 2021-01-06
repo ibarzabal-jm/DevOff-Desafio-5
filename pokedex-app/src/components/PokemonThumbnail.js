d2207ddba2edfb3441b05be572235303ee42035d
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2021-01-05 23:34:30 -0300

    Update PokemonThumbnail.js

--- pokedex-app/src/components/PokemonThumbnail.js
+++ pokedex-app/src/components/PokemonThumbnail.js
@@ -1,9 +1,9 @@
-import { Box, Heading, Stack, Text, Image } from "@chakra-ui/react";
 import React from "react";
+import { Heading, Stack, Text, Image } from "@chakra-ui/react";
 
 const PokemonThumbnail = ({ pokemon, onClick }) => {
   return (
-    <Box
+    <Stack
       color="white"
       backgroundColor={`${pokemon.types[0]}`}
       borderRadius={12}
@@ -11,32 +11,30 @@ const PokemonThumbnail = ({ pokemon, onClick }) => {
       onClick={onClick}
       cursor="pointer"
     >
-      <Stack>
-        <Stack direction="row" align="baseline" justifyContent="space-between">
-          <Heading textTransform="capitalize" fontSize="lg">
-            {pokemon.name}
-          </Heading>
-          <Text>{pokemon.id}</Text>
-        </Stack>
-        <Stack direction="row" justifyContent="space-between">
-          <Stack>
-            {pokemon.types.map((type) => (
-              <Text
-                key={type}
-                paddingX={2}
-                paddingY={1}
-                backgroundColor="rgba(255,255,255,0.2)"
-                borderRadius={9999}
-                width="fit-content"
-              >
-                {type}
-              </Text>
-            ))}
-          </Stack>
-          <Image maxWidth="90px" width="100%" src={pokemon.image}></Image>
-        </Stack>
+      <Stack direction="row" align="baseline" justifyContent="space-between">
+        <Heading textTransform="capitalize" fontSize="lg">
+          {pokemon.name}
+        </Heading>
+        <Text>#{pokemon.id.toString().padStart(3, "0")}</Text>
       </Stack>
-    </Box>
+      <Stack direction="row" justifyContent="space-between">
+        <Stack>
+          {pokemon.types.map((type) => (
+            <Text
+              key={type}
+              paddingX={2}
+              paddingY={1}
+              backgroundColor="rgba(255,255,255,0.2)"
+              borderRadius={9999}
+              width="fit-content"
+            >
+              {type}
+            </Text>
+          ))}
+        </Stack>
+        <Image maxWidth="100px" width={16} src={pokemon.image}></Image>
+      </Stack>
+    </Stack>
   );
 };
 
