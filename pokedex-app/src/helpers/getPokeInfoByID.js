9dc74f83a7b0e67033068f5c543b9333f8dd44a7
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-01-15 05:39:36 -0300

    Update getPokeInfoByID.js

--- pokedex-app/src/helpers/getPokeInfoByID.js
+++ pokedex-app/src/helpers/getPokeInfoByID.js
@@ -1,10 +1,25 @@
 import { getWeaknesses } from "./getWeaknesses";
 
+const moveFetch = async (url) => {
+  const resp = await fetch(url);
+  const { accuracy, damage_class, name, pp, power, type } = await resp.json();
+  const dataType = type.name;
+  const dataClass = damage_class.name;
+  return {
+    accuracy,
+    dataClass,
+    name,
+    power,
+    type: dataType,
+  };
+};
+
 const getFirstData = async (id) => {
   const resp = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
   const {
     abilities: abilitiesData,
     height,
+    moves,
     name,
     stats: statsData,
     types,
@@ -13,11 +28,24 @@ const getFirstData = async (id) => {
 
   const typesData = types.map(({ type }) => type.name);
 
+  const movesFilter = moves.filter(({ version_group_details }) =>
+    version_group_details.find(
+      ({ version_group }) => version_group.name === "firered-leafgreen"
+    )
+  );
+
+  const movesData = await Promise.all(
+    movesFilter.map(({ move }) => moveFetch(move.url))
+  );
+
+  console.log(movesData);
+
   return {
     abilities: abilitiesData.map(({ ability }) => ability.name),
     height: height / 10,
     id,
     image: `https://pokeres.bastionbot.org/images/pokemon/${id}.png`,
+    moves: movesData,
     name,
     stats: statsData.reduce(
       (obj, item) => ({ ...obj, [item.stat.name]: item.base_stat }),
