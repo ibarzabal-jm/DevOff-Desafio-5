ee1256c852a49f89db0b3f24cf836310e3cb432e
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-01-15 05:39:57 -0300

    Create TypeCombination.js

---
+++ pokedex-app/src/components/pages/TypeCombination.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CombinationContent from "../Combination/CombinationContent";
+import Header from "../Combination/Header";
+
+const TypeCombination = () => {
+  return (
+    <div>
+      <Header />
+      <CombinationContent />
+    </div>
+  );
+};
+
+export default TypeCombination;
