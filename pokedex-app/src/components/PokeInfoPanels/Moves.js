da7357aee0bdacccb8cc9071ff11c1bb02c52f22
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-01-20 05:09:46 -0300

    Update Moves.js

--- pokedex-app/src/components/PokeInfoPanels/Moves.js
+++ pokedex-app/src/components/PokeInfoPanels/Moves.js
@@ -1,40 +1,42 @@
 import React from "react";
-import { Badge, Box, Stack, Text } from "@chakra-ui/react";
+import { Badge, Box, Table, Tbody, Text, Tr } from "@chakra-ui/react";
 
 const Moves = ({ moves }) => {
   return (
-    <Stack>
-      <Box as="table" width="100%">
-        <thead>
+    <Table variant="striped" colorScheme="gray">
+      <thead align="center">
+        <tr>
           <th>name</th>
           <th>power</th>
           <th>type</th>
           <th>accuracy</th>
           <th>class</th>
-        </thead>
-        <tbody>
-          {moves.map((move) => (
-            <tr key={move.name}>
-              <Text align="center" as="td">
-                {move.name}
-              </Text>
-              <Text align="center" as="td">
-                {move.power}
-              </Text>
-              <Badge bg={move.type} as="td">
+        </tr>
+      </thead>
+      <Tbody>
+        {moves.map((move) => (
+          <Tr key={move.name}>
+            <Text align="center" textTransform="capitalize" as="td">
+              {move.name}
+            </Text>
+            <Text align="center" as="td">
+              {move.power ? move.power : `-`}
+            </Text>
+            <Box as="td" align="center" justifyContent="center">
+              <Badge bg={move.type} rounded={999} textAlign="center">
                 {move.type}
               </Badge>
-              <Text align="center" as="td">
-                {move.accuracy}
-              </Text>
-              <Text align="center" as="td">
-                {move.dataClass}
-              </Text>
-            </tr>
-          ))}
-        </tbody>
-      </Box>
-    </Stack>
+            </Box>
+            <Text align="center" as="td">
+              {move.accuracy ? move.accuracy : `-`}
+            </Text>
+            <Text align="center" textTransform="capitalize" as="td">
+              {move.dataClass}
+            </Text>
+          </Tr>
+        ))}
+      </Tbody>
+    </Table>
   );
 };
 
