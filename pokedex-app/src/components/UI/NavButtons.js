c96fac7c17403136df9680290e5ca94f2a4ed1cb
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-01-27 14:39:29 -0300

    Update NavButtons.js

--- pokedex-app/src/components/UI/NavButtons.js
+++ pokedex-app/src/components/UI/NavButtons.js
@@ -4,7 +4,7 @@ import { Link as ReachLink } from "react-router-dom";
 
 const NavButtons = () => {
   return (
-    <SimpleGrid gap={3} columns={2} padding={3}>
+    <SimpleGrid gap={3} columns={2} padding={3} margin={10}>
       <Stack
         align="center"
         bg="grass"
