5b1e052686678ad134ee156b3c4e8676ace5c198
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-01-27 14:39:42 -0300

    Update SearchPokemons.js

--- pokedex-app/src/components/Pages/SearchPokemons.js
+++ pokedex-app/src/components/Pages/SearchPokemons.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Heading, Stack } from "@chakra-ui/react";
+import { Heading, SimpleGrid, Stack } from "@chakra-ui/react";
 import { AddPokemon } from "../SearchPokemon/AddPokemon";
 import PokemonThumbnail from "../PokemonThumbnail";
 
@@ -10,9 +10,19 @@ const SearchPokemonsPage = () => {
     <Stack spacing={4} padding={10}>
       <Heading textAlign="center">What Pokemon are you looking for?</Heading>
       <AddPokemon setPokemons={setPokemons} />
-      {pokemons.map((pokemon) => (
-        <PokemonThumbnail pokemon={pokemon} />
-      ))}
+      {pokemons.length > 0 && (
+        <SimpleGrid
+          columns={2}
+          gap={3}
+          borderRadius={20}
+          bg="gray.200"
+          padding={2}
+        >
+          {pokemons.map((pokemon) => (
+            <PokemonThumbnail pokemon={pokemon} />
+          ))}
+        </SimpleGrid>
+      )}
     </Stack>
   );
 };
