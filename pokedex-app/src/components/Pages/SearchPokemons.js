6ea08f07f0cad3dbfa3e303e49b96135f33c481b
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-01-27 02:16:07 -0300

    Create SearchPokemons.js

---
+++ pokedex-app/src/components/Pages/SearchPokemons.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import { Heading, Stack } from "@chakra-ui/react";
+import { AddPokemon } from "../SearchPokemon/AddPokemon";
+import PokemonThumbnail from "../PokemonThumbnail";
+
+const SearchPokemonsPage = () => {
+  const [pokemons, setPokemons] = useState([]);
+
+  return (
+    <Stack spacing={4} padding={10}>
+      <Heading textAlign="center">What Pokemon are you looking for?</Heading>
+      <AddPokemon setPokemons={setPokemons} />
+      {pokemons.map((pokemon) => (
+        <PokemonThumbnail pokemon={pokemon} />
+      ))}
+    </Stack>
+  );
+};
+
+export default SearchPokemonsPage;
