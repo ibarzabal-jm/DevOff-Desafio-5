0a69698f119b76c311cda51872e32f03d29a8e7e
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-01-28 19:08:59 -0300

    Update Landing.js

--- pokedex-app/src/components/Pages/Landing.js
+++ pokedex-app/src/components/Pages/Landing.js
@@ -2,14 +2,15 @@ import React from "react";
 import Pokedex from "./Pokedex";
 import SearchPokemons from "./SearchPokemons";
 import NavButtons from "../UI/NavButtons";
+import { Box } from "@chakra-ui/react";
 
 const Landing = () => {
   return (
-    <>
+    <Box bg="#fff" margin={{ base: 0, md: 4 }} rounded={20}>
       <SearchPokemons />
       <NavButtons />
       <Pokedex />
-    </>
+    </Box>
   );
 };
 
