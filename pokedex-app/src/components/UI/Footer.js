84a069b81c0d89928ab2bd21e4f15bf865ebed26
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-01-28 04:36:36 -0300

    Update Footer.js

--- pokedex-app/src/components/UI/Footer.js
+++ pokedex-app/src/components/UI/Footer.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Stack } from "@chakra-ui/react";
 import Copyright from "./Copyright";
-import Repositorio from "./Repositorio";
 
 const Footer = () => {
   return (
     <Stack textAlign="center" justify="center" bg="black" paddingY={4}>
       <Copyright />
-      <Repositorio />
     </Stack>
   );
 };
