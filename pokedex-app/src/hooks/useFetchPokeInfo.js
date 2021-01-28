84aac51b963338831cc7c7ac9440d81ee670974a
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-01-27 21:15:05 -0300

    useRef update

--- pokedex-app/src/hooks/useFetchPokeInfo.js
+++ pokedex-app/src/hooks/useFetchPokeInfo.js
@@ -1,10 +1,15 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { getPokeInfoByID } from "../api/getPokeInfoByID";
 
 export const useFetchPokeInfo = (id) => {
+  const isMounted = useRef(true);
   const [state, setState] = useState({ data: [], loading: true });
 
-  
+  useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
 
   useEffect(() => {
     getPokeInfoByID(id).then((pokeData) =>
