d10de448b5804af6ec1d83975d86b2ecb115fec5
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-01-27 21:15:31 -0300

    Update Copyright.js

--- pokedex-app/src/components/UI/Copyright.js
+++ pokedex-app/src/components/UI/Copyright.js
@@ -1,17 +1,15 @@
 import React from "react";
 import { Text } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
 
 const Copyright = ({ textcolor, position }) => {
   return (
-    <Text textAlign="center" textColor="white">
-      {" PokdexApp  Copyright © "}
-      <Link
-        to="/"
-        onClick={() => window.open(`https://juanmaportfolio.netlify.app/`)}
-      >
-        Juan Manuel Ibarzabal Salles
-      </Link>{" "}
+    <Text
+      cursor="pointer"
+      onClick={() => window.open(`https://juanmaportfolio.netlify.app/`)}
+      textAlign="center"
+      textColor="white"
+    >
+      PokdexApp Copyright © Juan Manuel Ibarzabal Salles{" "}
       {new Date().getFullYear()}
       {"."}
     </Text>
