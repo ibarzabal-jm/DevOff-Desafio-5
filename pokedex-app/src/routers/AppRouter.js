69f0fdb5f88c2cc69a467886fb5b6fc6de1fe9e4
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-01-28 16:26:07 -0300

    Update AppRouter.js

--- pokedex-app/src/routers/AppRouter.js
+++ pokedex-app/src/routers/AppRouter.js
@@ -10,29 +10,37 @@ import Pokedex from "../components/Pages/Pokedex";
 import SearchPokemons from "../components/Pages/SearchPokemons";
 import SearchMoves from "../components/Pages/SearchMoves";
 import TypeCombination from "../components/Pages/TypeCombination";
+import { Flex } from "@chakra-ui/react";
 
 export const AppRouter = () => {
   return (
     <Router>
       <>
         <Navbar />
-        <Switch>
-          <Route exact path="/search">
-            <SearchPokemons />
-          </Route>
-          <Route exact path="/moves">
-            <SearchMoves />
-          </Route>
-          <Route exact path="/combination">
-            <TypeCombination />
-          </Route>
-          <Route exact path="/pokedex">
-            <Pokedex />
-          </Route>
-          <Route path="/">
-            <Landing />
-          </Route>
-        </Switch>
+        <Flex
+          direction="column"
+          alignItems="stretch"
+          maxWidth="1920px"
+          minH="100vh"
+        >
+          <Switch>
+            <Route exact path="/search">
+              <SearchPokemons />
+            </Route>
+            <Route exact path="/moves">
+              <SearchMoves />
+            </Route>
+            <Route exact path="/combination">
+              <TypeCombination />
+            </Route>
+            <Route exact path="/pokedex">
+              <Pokedex />
+            </Route>
+            <Route path="/">
+              <Landing />
+            </Route>
+          </Switch>
+        </Flex>
         <Footer />
       </>
     </Router>
