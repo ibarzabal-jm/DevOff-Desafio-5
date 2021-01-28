48fbc2ded446352510d586579de8c64eb811254e
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-01-28 19:08:44 -0300

    Update Navbar.js

--- pokedex-app/src/components/UI/Navbar.js
+++ pokedex-app/src/components/UI/Navbar.js
@@ -9,13 +9,55 @@ const Navbar = () => {
       isInline
       alignItems="center"
       height={12}
-      justifyContent={{ base: "center" }}
+      justifyContent={{ base: "center", md: "space-between" }}
       bg="gray.900"
       zIndex={200}
     >
       <Stack isInline>
         <Button colorScheme="gray.50" variant="solid" as={ReachLink} to="/home">
           PokedexApp
+        </Button>
+      </Stack>
+      <Stack isInline mr={2} display={{ base: "none", md: "inline-flex" }}>
+        <Button
+          size="sm"
+          color="white"
+          bg="grass"
+          _hover={{ color: "black", opacity: 0.8 }}
+          as={ReachLink}
+          to="/pokedex"
+        >
+          Pokedex
+        </Button>
+        <Button
+          size="sm"
+          color="white"
+          bg="fire"
+          _hover={{ color: "black", opacity: 0.8 }}
+          as={ReachLink}
+          to="/moves"
+        >
+          Moves
+        </Button>
+        <Button
+          size="sm"
+          color="white"
+          bg="water"
+          _hover={{ color: "black", opacity: 0.8 }}
+          as={ReachLink}
+          to="/search"
+        >
+          Find Pokemon
+        </Button>
+        <Button
+          size="sm"
+          color="white"
+          bg="poison"
+          _hover={{ color: "black", opacity: 0.8 }}
+          as={ReachLink}
+          to="/combination"
+        >
+          Types Weaks
         </Button>
       </Stack>
     </Stack>
