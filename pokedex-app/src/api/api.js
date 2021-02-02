1acc370fb34ecec59e329d75d8442ed2821ff22f
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-02-02 03:59:33 -0300

    Update api.js

--- pokedex-app/src/api/api.js
+++ pokedex-app/src/api/api.js
@@ -1,7 +1,6 @@
 import { URL_POKEMON, PAGE_SIZE } from "./settingsURL";
-import { getMiniDataPokemonByID } from "./getMiniDataPokemonByID";
 import { getPokeInfoByID } from "./getPokeInfoByID";
-import { getMiniDataPokemonByName } from "./getMiniDataByName";
+import { getMiniData } from "./getMiniData";
 
 export const api = {
   list: (page) => {
@@ -11,17 +10,12 @@ export const api = {
       .then((res) => res.json())
       .then((response) =>
         Promise.all(
-          response.results.map((pokemon) =>
-            getMiniDataPokemonByName(pokemon.name)
-          )
+          response.results.map((pokemon) => getMiniData(pokemon.name))
         )
       );
   },
-  miniDataByName: (name) => {
-    return getMiniDataPokemonByName(name);
-  },
-  miniData: (id) => {
-    return getMiniDataPokemonByID(id);
+  miniData: (key) => {
+    return getMiniData(key);
   },
   fullData: (id) => {
     return getPokeInfoByID(id);
@@ -34,6 +28,6 @@ export const api = {
       .fill(true)
       .map((_, index) => index)
       .slice(1)
-      .map((id) => getMiniDataPokemonByID(PAGE_SIZE * page + id))
+      .map((id) => getMiniData(PAGE_SIZE * page + id))
   );
 }, */
