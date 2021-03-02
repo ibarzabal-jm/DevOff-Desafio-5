4de51ce3bc804e8600230787b15397109d4fa9aa
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-03-02 02:33:46 -0300

    Update Header.js

--- pokedex-app/src/components/TypesCombination/Header.js
+++ pokedex-app/src/components/TypesCombination/Header.js
@@ -3,7 +3,7 @@ import { Heading, Stack } from "@chakra-ui/react";
 
 const Header = () => {
   return (
-    <Stack margin={{ base: 3, md: 5 }} padding={{ base: 0, md: 1 }}>
+    <Stack padding={{ base: 0, md: 1 }}>
       <Heading as="h3">Pokémon Type Calculator</Heading>
       <Heading size="sm" as="h6" fontWeight="normal" color="gray.500">
         Choose Types and Calculate Weaknesses
