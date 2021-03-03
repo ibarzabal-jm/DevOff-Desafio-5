f152b4c77e19be0e2dcc7ec27870781493f30f28
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-03-03 00:51:48 -0300

    Update TypeButton.js

--- pokedex-app/src/components/TypesCombination/TypeButton.js
+++ pokedex-app/src/components/TypesCombination/TypeButton.js
@@ -1,25 +1,27 @@
 import React from "react";
-import { Image, Tag, TagLabel } from "@chakra-ui/react";
+import { Tag, TagLabel, Box } from "@chakra-ui/react";
 
 const TypeButton = ({ type, selectedType, changeSelectedType }) => {
   return (
     <Tag
       as="button"
-      bg={type !== null ? type : "grey"}
+      bg="white"
+      border={type === selectedType ? "2px solid" : "1px solid #ccc"}
       cursor="pointer"
       margin={1}
-      rounded={999}
+      rounded={20}
       value={type}
-      w="90px"
+      w="95px"
       onClick={() => changeSelectedType(type)}
     >
-      <Image
-        mr={3}
+      <Box
+        rounded={99999}
+        bg={type !== null ? type : "grey"}
+        mr={1}
         ml={-1}
-        src="../assets/ui/pokeball.svg"
-        opacity={type !== selectedType ? "0.1" : "1"}
-        width={4}
-      />
+        w={4}
+        h={4}
+      ></Box>
       <TagLabel textTransform="capitalize" fontWeight="bold">
         {type === null ? "None" : type}
       </TagLabel>
