71d081404d3945f0d319e180cc98f0ace06e4075
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-03-03 00:51:15 -0300

    add toolbar

--- pokedex-app/src/components/pages/TypeCombination.js
+++ pokedex-app/src/components/pages/TypeCombination.js
@@ -1,19 +1,18 @@
 import React from "react";
 import { Heading, Stack } from "@chakra-ui/react";
-import GoHomeButton from "../UI/GoHomeButton";
 import CombinationContainer from "../TypesCombination/CombinationContainer";
+import ToolBarPages from "../UI/ToolBarPages";
 
 const TypeCombinationPage = () => {
   return (
     <Stack
       alignContent="center"
       bg="white"
-      margin={{ base: 0, sm: 1, md: 4 }}
       padding={{ base: 0, md: 4 }}
       rounded={30}
       spacing={3}
     >
-      <GoHomeButton componentPathName="/combination" />
+      <ToolBarPages />
 
       <Stack spacing={0}>
         <Heading>Pokémon Type Calculator</Heading>
