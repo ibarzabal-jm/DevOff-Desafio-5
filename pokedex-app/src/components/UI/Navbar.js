dfa4e84636c13810f763bb1b4ac08e796d857e52
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-03-03 00:48:47 -0300

    delete buttons

--- pokedex-app/src/components/UI/Navbar.js
+++ pokedex-app/src/components/UI/Navbar.js
@@ -9,67 +9,13 @@ const Navbar = () => {
       isInline
       alignItems="center"
       height={12}
-      justifyContent={{ base: "center", md: "space-between" }}
+      justifyContent="center"
       bg="gray.900"
       zIndex={200}
     >
-      <Stack isInline>
-        <Button colorScheme="gray.50" variant="solid" as={ReachLink} to="/home">
-          PokedexApp
-        </Button>
-      </Stack>
-      <Stack isInline mr={2} display={{ base: "none", md: "inline-flex" }}>
-        <Button
-          size="sm"
-          color="white"
-          bg="grass"
-          _hover={{ color: "black", opacity: 0.8 }}
-          as={ReachLink}
-          to="/pokedex"
-        >
-          Pokedex
-        </Button>
-        <Button
-          size="sm"
-          color="white"
-          bg="fire"
-          _hover={{ color: "black", opacity: 0.8 }}
-          as={ReachLink}
-          to="/moves"
-        >
-          Moves
-        </Button>
-        <Button
-          size="sm"
-          color="white"
-          bg="water"
-          _hover={{ color: "black", opacity: 0.8 }}
-          as={ReachLink}
-          to="/search"
-        >
-          Find Pokemon
-        </Button>
-        <Button
-          size="sm"
-          color="white"
-          bg="ground"
-          _hover={{ color: "black", opacity: 0.8 }}
-          as={ReachLink}
-          to="/quiz"
-        >
-          Who's that Pokemon?
-        </Button>
-        <Button
-          size="sm"
-          color="white"
-          bg="poison"
-          _hover={{ color: "black", opacity: 0.8 }}
-          as={ReachLink}
-          to="/combination"
-        >
-          Types Weaks
-        </Button>
-      </Stack>
+      <Button colorScheme="gray.50" variant="solid" as={ReachLink} to="/home">
+        PokedexApp
+      </Button>
     </Stack>
   );
 };
