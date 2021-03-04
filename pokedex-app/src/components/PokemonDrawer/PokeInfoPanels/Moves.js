305c32287150fcf02632f83211df3a199df1c387
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-03-04 02:56:36 -0300

    add sort

--- pokedex-app/src/components/PokemonDrawer/PokeInfoPanels/Moves.js
+++ pokedex-app/src/components/PokemonDrawer/PokeInfoPanels/Moves.js
@@ -1,20 +1,68 @@
-import React from "react";
-import { Badge, Box, Table, Tbody, Text, Tr } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { Badge, Box, Stack, Table, Tbody, Text, Tr } from "@chakra-ui/react";
+import {
+  sortTableAscending,
+  sortTableDescending,
+} from "../../../helpers/sortTableMoves";
+import { TriangleDownIcon, TriangleUpIcon } from "@chakra-ui/icons";
 
 const Moves = ({ moves }) => {
+  const headers = ["name", "power", "type", "accuracy", "class"];
+
+  const [sortedMoves, setSortedMoves] = useState(moves);
+  const [sorted, setSorted] = useState("name");
+  const [ascending, setAscending] = useState(true);
+
+  useEffect(() => {
+    setSortedMoves(
+      ascending
+        ? sortTableAscending(moves, sorted)
+        : sortTableDescending(moves, sorted)
+    );
+  }, [sorted, moves, ascending]);
+
+  const handleSort = (sorting) => {
+    if (sorting === sorted) {
+      setAscending(!ascending);
+    } else {
+      setAscending(true);
+      setSorted(sorting);
+    }
+  };
+
   return (
-    <Table variant="striped" colorScheme="gray" w="100%">
-      <thead align="center">
+    <Table variant="striped" align="center" colorScheme="gray" size="sm">
+      <thead>
         <tr>
-          <th>name</th>
-          <th>power</th>
-          <th>type</th>
-          <th>accuracy</th>
-          <th>class</th>
+          {headers.map((header) => (
+            <th key={header} onClick={() => handleSort(header)}>
+              <Stack
+                isInline
+                justify="center"
+                cursor="pointer"
+                align="center"
+                spacing={0}
+                width="100%"
+              >
+                <Text>{header}</Text>
+
+                {header === sorted &&
+                  (ascending ? (
+                    <TriangleDownIcon
+                      aria-label="sorted descending"
+                      h={2}
+                      w={2}
+                    />
+                  ) : (
+                    <TriangleUpIcon aria-label="sorted ascending" h={2} w={2} />
+                  ))}
+              </Stack>
+            </th>
+          ))}
         </tr>
       </thead>
       <Tbody>
-        {moves.map((move) => (
+        {sortedMoves.map((move) => (
           <Tr key={move.name}>
             <Text align="center" textTransform="capitalize" as="td">
               {move.name}
